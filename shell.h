03f00d216c25a8b5314657a0e0495275f0db67bd
root <houssam.khaled@lau.edu>
2025-02-19 23:32:30 +0000

    files

---
+++ shell.h
@@ -0,0 +1,20 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <errno.h>
+
+
+extern char **environ;
+
+void shell_loop(void);
+void exec_cmd(char *input);
+
+
+
+
+#endif
