85aa00fdac6a5232d66b01e611d803f1384e6740
root <houssam.khaled@lau.edu>
2025-02-23 20:26:00 +0000

    done?

--- shell.c
+++ shell.c
@@ -115,7 +115,7 @@ exit(127);
 else
 {
 path = get_path_from_env(env);
-if (path == NULL || strlen(path) == 0)
+if (path == NULL)
 {
 fprintf(stderr, "%s: 1: %s: found\n", argv[0], args[0]);
 free(input);
